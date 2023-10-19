8c07a61c2cd43c5bc0e5ce56d3123a305d76d6f3
André Paulino <andrepaulinobtw@gmail.com>
2023-10-19 10:47:01 -0300

    Aula 3

--- src/com/andrepaulino/OrdenaString.java
+++ src/com/andrepaulino/OrdenaString.java
@@ -1,6 +1,7 @@
 package com.andrepaulino;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaString {
@@ -12,11 +13,14 @@ public class OrdenaString {
 
         // Consumer<String> consumidor = new ImprimeNaLinha();
         // palavras.forEach(consumidor);
-        palavras.forEach(s -> System.out.println(s));
+        // palavras.forEach(s -> System.out.println(s));
+        palavras.forEach(System.out::println);
 
         // ComparadorPorTamanho comparador = new ComparadorPorTamanho();
         // palavras.sort(comparador);
-        palavras.sort((s1, s2) -> s1.length() - s2.length());
+        // palavras.sort((s1, s2) -> s1.length() - s2.length());
+        // palavras.sort(Comparator.comparing(s -> s.length()));
+        palavras.sort(Comparator.comparing(String::length));
         System.out.println(palavras);
     }
 }
